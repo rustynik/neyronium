352b159d0faed7ec5f908ad139abde4ab35e4fec
vaisvanara <andrei.nikolaew@gmail.com>
2018-09-22 22:59:05 +0300

    bug fix

--- routes/details.js
+++ routes/details.js
@@ -3,8 +3,8 @@ const express = require('express'),
 
 router.get('/:courseId', function(req, res, next) {
     req.courses.findById(req.params.courseId, (err, course) => {
-        if (err) next(err);
-        if (!course) next({ status: 404, message: `course ${ req.params.courseId } not found`});
+        if (err) return next(err);
+        if (!course) return next({ status: 404, message: `course ${ req.params.courseId } not found`});
 
         res.render('details', { course, title: course.title });
     })    
