6c401938ee16078968be8e7b1de962e773ea41e3
vaisvanara <andrei.nikolaew@gmail.com>
2018-09-23 13:56:10 +0300

    styles

--- services/settings.js
+++ services/settings.js
@@ -5,9 +5,9 @@ const fs = require('fs'),
  * gets application settings by merging basic settings file located at root path 
  * and (optionally) another settings path designated by configuration name 
  * If no configuration name is provided, basic settings are returned
- * @param {*} rootPath
- * @param {*} configurationName
- * @returns settings object
+ * @param {String} rootPath
+ * @param {String} configurationName
+ * @returns {object} settings object
  */
 function read(rootPath, configurationName) {
 
@@ -16,11 +16,22 @@ function read(rootPath, configurationName) {
         configurationName ? readJson(makePath(rootPath, configurationName)) : {}
     );
 }
-
+/**
+ * read file as json
+ *
+ * @param {String} path
+ * @returns json
+ */
 function readJson(path) {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
-
+/**
+ * make settings path based on configuration name
+ * configuration name, if any, is inserted as settings.{confName}.json
+ * @param {String} rootPath
+ * @param {String} configurationName
+ * @returns
+ */
 function makePath(rootPath, configurationName) {
     const fileName = configurationName ? `settings.${ configurationName }.json` : 'settings.json';
     return combine(rootPath, fileName);
